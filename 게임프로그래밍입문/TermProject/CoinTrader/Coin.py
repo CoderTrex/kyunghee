14a5f7ec885d89c2f343502106b1af5506edc54e
정은성 <80687043+CoderTrex@users.noreply.github.com>
2022-11-03 15:48:11 +0900

    game

--- 게임프로그래밍입문/TermProject/CoinTrader/Coin.py
+++ 게임프로그래밍입문/TermProject/CoinTrader/Coin.py
@@ -17,12 +17,28 @@ from matplotlib import ticker
 
 # 초기 자본금 / 평가 금액 / 가지고 있는 코인의 이름과 금액
 have_Money = 500000000
-have_coin = {1:0, 2:0, 3:0, 4:0, 5:0} # 코인 넘버 : 코인 개수
-Valuation_coin = 0
-
+have_coin = {'1':0, '2':0, '3':0, '4':0, '5':0} # 코인 넘버 : 코인 개수
 
 # 구매 가능한 코인의 목록
-ticker_name = ["KRW-BTC", "kRW-XRP", "kRW-DOGE", "kRW-WEMIX", "kRW-ETH"]
+ticker_name = ["Blank", "KRW-BTC", "kRW-XRP", "kRW-DOGE", "kRW-WEMIX", "kRW-ETH"]
+Valuation_coin = 0
+
+def coin_list():
+    for name, count in have_coin.items(): 
+        Val = 0
+        coin_current_price = pyupbit.get_current_price(ticker_name[int(name)])
+        # 코인 평가 금액 +=  
+        Val += coin_current_price * count 
+        print("coin:{0}\nprice:{1}".format(ticker_name[int(name)], Val))
+    print()
+
+def check_coin_price():
+    Val = 0
+    for name, count in have_coin.items(): 
+        coin_current_price = pyupbit.get_current_price(ticker_name[int(name)])
+        # 코인 평가 금액 +=  
+        Val += coin_current_price * count 
+    return Val
 
 def current_coin_prince():
     for i  in ticker_name:
@@ -31,17 +47,12 @@ def current_coin_prince():
 while(True):
     # 현재 보유하고 있는 코인의 양을 출력함   
     # 가지고 있는 코인의 이름과 개수를 출력함
-    for name, count in have_coin:    
-        coin_current_price = pyupbit.get_current_price(ticker_name[name])
-        # 코인 평가 금액 +=  
-        Valuation_coin += coin_current_price * count 
-        print("coin:{0}\nprice:{1}".format(ticker_name[name], Valuation_coin))
-        
+    Valuation_coin = check_coin_price()
     print("Valuation amount : {}".format(Valuation_coin + have_Money))
     print("hand money       : {}".format(have_Money))
     print ("\n\n")
     print("Enter the number to be done\n")
-    print("1. buy\t2. sell\t3. infomation\t 4. Balance Check")
+    print("1. buy  2. sell  3. infomation  4. Balance Check")
     behavior = int(input())
 
     # 구매 작업
@@ -61,13 +72,12 @@ while(True):
                 break
             else:
                 purchase_coin_price = pyupbit.get_current_price(ticker_name[buy_ticker_num])
-                number_coin = purchase_coin_price / purchase_amount
-                have_coin.append((buy_ticker_num, number_coin))
-                have_Money -= purchase_amount
+                # 구매코인의 개수 = 구매하려는 금액 / 구매 코인의 가격
+                number_coin = purchase_amount / purchase_coin_price
+                coin_amount = have_coin.get(str(buy_ticker_num))
+                have_coin.update({str(buy_ticker_num): number_coin + coin_amount})
+                have_Money -= (purchase_amount)
                 break
-            
-    
-    # 현재가 가져와서 출력
-    # def now_money():
-    #     for i in ticker_name:
-    #         currnet_price = pyupbit.get_current_price(i)
+
+    if (behavior == 4):
+        coin_list()
