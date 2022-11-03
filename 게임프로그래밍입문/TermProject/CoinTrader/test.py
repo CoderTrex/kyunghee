1ca9fc5371f55606e8a566d014d6120141cb3cc9
정은성 <80687043+CoderTrex@users.noreply.github.com>
2022-11-03 16:23:04 +0900

    data

--- 게임프로그래밍입문/TermProject/CoinTrader/test.py
+++ 게임프로그래밍입문/TermProject/CoinTrader/test.py
@@ -2,17 +2,3 @@ from schedule import *
 import schedule
 import time
  
-def job():
-    print("Schedule Test")
-
-schedule.every(1).second.do(job)
-
-
-cocos.
-count = 0
-
-while True:
-    schedule.run_pending()
-    count += 1
-    time.sleep(1)
-    print(count)
