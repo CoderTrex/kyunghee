eb8ce8c4db36f9371bfe9e56988c21685302d447
정은성 <80687043+CoderTrex@users.noreply.github.com>
2022-12-12 12:42:04 +0900

    game

--- 게임프로그래밍입문/실습코드모음/Chapter 4/code/obstacles.py
+++ 게임프로그래밍입문/실습코드모음/Chapter 4/code/obstacles.py
@@ -39,32 +39,48 @@ class Actor(cocos.cocosnode.CocosNode):
         self.schedule(self.update)
 
     def update(self, dt):
+        # 타겟이 없으면 움직이지 않는다.
         if self.target is None:
             return
+        # 타겟과 현재의 x, y값을 토대로 움직인다.
         distance = self.target - eu.Vector2(self.x, self.y)
+        # 움직여야 하는 곳의 위치 및 방향을 구한다.
         steering = distance * self.speed - self.velocity
+        # 피해야 하는 힘을 준다.
         steering += self.avoid_force()
-        steering = truncate(steering, self.max_force)
-        self.velocity = truncate(self.velocity + steering, 
-                                 self.max_velocity)
+        # 정규화
+        steering = truncate(steering, self.max_force) 
+        self.velocity = truncate(self.velocity + steering, self.max_velocity)
+        # 위치 갱신
         self.position += self.velocity * dt
 
     def avoid_force(self):
         avoid = eu.Vector2(0, 0)
+        # 최대 속도를 곱해주고 최대 속력으로 나누어 준다. -> 이동해야 하는 방향
         ahead = self.velocity * self.max_ahead / self.max_velocity
+        # 내적을 해준다.
         l = ahead.dot(ahead)
         if l == 0:
             return avoid
         closest, closest_dist = None, None
         for obj in Obstacle.instances:
+            # 방해물 위치와 자신의 위치를 빼서 백터를 구한다
             w = eu.Vector2(obj.x - self.x, obj.y - self.y)
+            # w의 내적을 구한다.
             t = ahead.dot(w)
+            # 만약 이것이 0과 1사이의 값이면
             if 0 < t < l:
+                # 내적값과 속력을 곱한 값, 즉 새롭게 갱신되어 이동해야 하는 값과 현재 위치를 더한다. 
                 proj = self.position + ahead * t / l
+                # 이것을 방해물 위치와 빼주고 절대값을 씌워 거리를 구한다.
                 dist = abs(obj.position - proj)
-                if dist < obj.radius and \
-                   (closest is None or dist < closest_dist):
+                
+                # 자신과의 거리가 방해물의 반지름(영향권) 
+                # and 가장 가까운 거리보다 더 작은 위치로 갱신이 되면 영향권을 바꾼다. (closet is none 은 초기값)
+                if (dist < obj.radius) and (closest is None or dist < closest_dist):
                     closest, closest_dist = obj.position, dist
+        
+        # 가장 가까운 방해물이 없다면 그냥 곱한다.
         if closest is not None:
             avoid = self.position + ahead - closest
             avoid = avoid.normalized() * self.max_avoid_force
--- 게임프로그래밍입문/실습코드모음/Chapter 4/code/wander.py
+++ 게임프로그래밍입문/실습코드모음/Chapter 4/code/wander.py
@@ -24,18 +24,22 @@ class Actor(cocos.cocosnode.CocosNode):
         self.max_velocity = 50
         self.add(ps.Sun())
         self.schedule(self.update)
+        
 
     def update(self, dt):
-        circle_center = self.velocity.normalized() * \
-                        self.circle_distance
+        # 정규화를 해준다. 값의 크기에 따라서 영향을 받지 않기 위해서 이다.
+        circle_center = self.velocity.normalized() * self.circle_distance
+        # 현재 각도에 따라서 dy dx를 구해준다.
         dx = math.cos(self.wander_angle)
         dy = math.sin(self.wander_angle)
+        # 이동할 위치의 가중치를 구한다.
         displacement = eu.Vector2(dx, dy) * self.circle_radius
-        self.wander_angle += (random.random() - 0.5) * \
-                             self.angle_change
+        # 랜덤 값의 위치로 이동한다. -0.5~0.5 사이 값을 가진다. 
+        self.wander_angle += (random.random() - 0.5) * self.angle_change
+        # 이동할 위치와 현재의 중심 위치를 더한 값을 구하고 정규화한다.
         self.velocity += circle_center + displacement
-        self.velocity = truncate(self.velocity, 
-                                 self.max_velocity)
+        self.velocity = truncate(self.velocity, self.max_velocity)
+        # 위치를 dt값과 속력을 곱한 값을 위치로 넣는다.
         self.position += self.velocity * dt
         self.position = (self.x % 640, self.y % 480)
 
